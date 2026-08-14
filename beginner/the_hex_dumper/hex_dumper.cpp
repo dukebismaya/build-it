9906c6f05631818e8c313fbfbc438fde2b9ffc88
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-08-14 20:52:54 +0530

    add: hex_dumper

--- beginner/the_hex_dumper/hex_dumper.cpp
+++ beginner/the_hex_dumper/hex_dumper.cpp
@@ -1,6 +1,18 @@
+#include <cstdint>
+#include <iomanip>
 #include <iostream>
 #include <vector>
 
-void printHexDump(const std::vector<uint8_t> &buffer);
+void printHexDump(const std::vector<uint8_t> &buffer) {
+  for (const auto packet : buffer) {
+    std::cout << std::setw(2) << std::setfill('0') << std::hex
+              << static_cast<int>(packet) << " ";
+  }
+  std::cout << std::setfill(' ');
+}
 
-int main() { return 0; }
+int main() {
+  std::vector<uint8_t> dummyPacket{255, 10, 128, 0, 42};
+  printHexDump(dummyPacket);
+  return 0;
+}
