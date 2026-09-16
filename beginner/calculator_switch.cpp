a88353b55f682d22ce4ecb8bc85effb3d4a24168
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-09-16 21:52:06 +0530

    Add calculator_switch program

---
+++ beginner/calculator_switch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    char op;
+    float num1, num2;
+
+    std::cout << "Enter operator (+, -, *, /): ";
+    std::cin >> op;
+    std::cout << "Enter two operands: ";
+    std::cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            std::cout << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
+            break;
+        case '-':
+            std::cout << num1 << " - " << num2 << " = " << num1 - num2 << "\n";
+            break;
+        case '*':
+            std::cout << num1 << " * " << num2 << " = " << num1 * num2 << "\n";
+            break;
+        case '/':
+            std::cout << num1 << " / " << num2 << " = " << num1 / num2 << "\n";
+            break;
+        default:
+            std::cout << "Error! operator is not correct\n";
+            break;
+    }
+
+    return 0;
+}
