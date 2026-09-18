1cd032cd3b87669645cba92b7536770f8e50d473
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-09-18 21:29:49 +0530

    Add Rabin-Karp algorithm program

---
+++ intermediate/rabin_karp_algorithm.cpp
@@ -0,0 +1,56 @@
+/*
+ * Problem Description:
+ * Implement the Rabin-Karp Algorithm for Pattern Searching.
+ * It uses hashing to find an exact match of a pattern string in a text string.
+ */
+
+#include <iostream>
+#include <string>
+
+#define d 256
+
+using namespace std;
+
+void search(string pat, string txt, int q) {
+    int M = pat.length();
+    int N = txt.length();
+    int i, j;
+    int p = 0;
+    int t = 0; 
+    int h = 1;
+    
+    for (i = 0; i < M - 1; i++)
+        h = (h * d) % q;
+        
+    for (i = 0; i < M; i++) {
+        p = (d * p + pat[i]) % q;
+        t = (d * t + txt[i]) % q;
+    }
+    
+    for (i = 0; i <= N - M; i++) {
+        if (p == t) {
+            for (j = 0; j < M; j++) {
+                if (txt[i + j] != pat[j])
+                    break;
+            }
+            if (j == M)
+                cout << "Pattern found at index " << i << "\n";
+        }
+        
+        if (i < N - M) {
+            t = (d * (t - txt[i] * h) + txt[i + M]) % q;
+            if (t < 0)
+                t = (t + q);
+        }
+    }
+}
+
+int main() {
+    string txt = "GEEKS FOR GEEKS";
+    string pat = "GEEK";
+    
+    int q = 101; 
+    search(pat, txt, q);
+    
+    return 0;
+}
