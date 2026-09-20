87903345b9beefcc92f1c2f474186e4a1fefe34a
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-09-20 17:38:29 +0530

    Add print pyramid program

---
+++ beginner/print_pyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int space, rows;
+
+    std::cout <<"Enter number of rows: ";
+    std::cin >> rows;
+
+    for(int i = 1, k = 0; i <= rows; ++i, k = 0) {
+        for(space = 1; space <= rows-i; ++space) {
+            std::cout <<"  ";
+        }
+
+        while(k != 2*i-1) {
+            std::cout << "* ";
+            ++k;
+        }
+        std::cout << "\n";
+    }    
+    return 0;
+}
