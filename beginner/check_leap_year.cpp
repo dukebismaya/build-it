3a6f4c91a173253e2f74172bb26ffc11e5572279
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-09-20 17:38:28 +0530

    Add check leap year program

---
+++ beginner/check_leap_year.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int year;
+
+    std::cout << "Enter a year: ";
+    std::cin >> year;
+
+    if (year % 400 == 0) {
+        std::cout << year << " is a leap year.\n";
+    }
+    else if (year % 100 == 0) {
+        std::cout << year << " is not a leap year.\n";
+    }
+    else if (year % 4 == 0) {
+        std::cout << year << " is a leap year.\n";
+    }
+    else {
+        std::cout << year << " is not a leap year.\n";
+    }
+
+    return 0;
+}
