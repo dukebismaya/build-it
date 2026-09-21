8d064b2068fe558fae7e5e0561468eb1e6db323a
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-09-21 20:49:48 +0530

    Add Shell Sort program

---
+++ intermediate/shell_sort.cpp
@@ -0,0 +1,43 @@
+/*
+ * Problem Description:
+ * Implement Shell Sort algorithm.
+ * Shell Sort is mainly a variation of Insertion Sort. In insertion sort, we move elements only one position ahead. 
+ * When an element has to be moved far ahead, many movements are involved. The idea of shellSort is to allow exchange of far items.
+ */
+
+#include  <iostream> 
+using namespace std; 
+  
+int shellSort(int arr[], int n) { 
+    for (int gap = n/2; gap > 0; gap /= 2) { 
+        for (int i = gap; i < n; i += 1) { 
+            int temp = arr[i]; 
+            int j;             
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+                arr[j] = arr[j - gap]; 
+            arr[j] = temp; 
+        } 
+    } 
+    return 0; 
+} 
+  
+void printArray(int arr[], int n) { 
+    for (int i=0; i<n; i++) 
+        cout << arr[i] << " "; 
+} 
+  
+int main() { 
+    int arr[] = {12, 34, 54, 2, 3}, i; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+  
+    cout << "Array before sorting: \n"; 
+    printArray(arr, n); 
+  
+    shellSort(arr, n); 
+  
+    cout << "\nArray after sorting: \n"; 
+    printArray(arr, n); 
+    cout << "\n";
+  
+    return 0; 
+}
