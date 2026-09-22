a4c5ee9205836f47ac5162be7a7a8a793dc39ded
devbismaya <179898597+dukebismaya@users.noreply.github.com>
2026-09-22 23:47:26 +0530

    Add remove vowels program

---
+++ beginner/remove_vowels.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str, result = "";
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u' && str[i] != 'A' &&
+            str[i] != 'E' && str[i] != 'I' && str[i] != 'O' &&
+            str[i] != 'U') {
+            result += str[i];
+        }
+    }
+
+    cout << "String without vowels: " << result << endl;
+
+    return 0;
+}
